7965074e56af35d23dd02c983ffd84f3d1bfa222
abinayagreen <abinayarrr7@gmail.com>
2020-08-08 16:01:47 +0530

    Abin updating code

--- src/test/java/org/git/GItPush.java
+++ src/test/java/org/git/GItPush.java
@@ -5,7 +5,7 @@ public class GItPush {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Success");
+		System.out.println("Abin updating code");
 
 	}
 
